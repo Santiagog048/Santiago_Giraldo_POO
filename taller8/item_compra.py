78a605b80892a1e67f77c7ff02f61c29e823b9d3
Santiagog048 <126509458+Santiagog048@users.noreply.github.com>
2023-10-17 23:32:25 -0500

    Delete taller8 directory

--- taller8/item_compra.py
+++
@@ -1,8 +0,0 @@
-class ItemCompra:
-    def __init__(self, libro, cantidad):
-        self.libro = libro
-        self.cantidad = cantidad
-
-    def calcular_subtotal(self):
-        return self.libro.precio * self.cantidad
-
--- taller8/libro_error.py
+++
@@ -1,6 +0,0 @@
-from libro import Libro
-
-class LibroError(Exception):
-    def __init__(self, libro: Libro):
-        self.libro = libro
-
